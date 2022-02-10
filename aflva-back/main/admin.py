d63a33b3794bc4a140f499e7e3e818195912f4f8
Nidzhat Agalarov <nagalarov@emphasoft.com>
2022-02-10 04:19:31 +0300

    added penalties

--- aflva-back/main/admin.py
+++ aflva-back/main/admin.py
@@ -68,9 +68,6 @@ class PenaltyAdmin(admin.ModelAdmin):
     model = Penalty
     list_display = ('id', 'name', 'rate')
 
-    def has_add_permission(self, request):
-        return False
-
     # def has_change_permission(self, request, obj=None):
     #     return False
     def has_delete_permission(self, request, obj=None):
