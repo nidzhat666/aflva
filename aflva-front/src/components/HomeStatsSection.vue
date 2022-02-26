24432d079ebfb2be35d1816a5d6dc88569bf52ab
Nidzhat Agalarov <nagalarov@emphasoft.com>
2022-02-26 15:44:03 +0300

    front init

---
+++ aflva-front/src/components/HomeStatsSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="stats mt-3 text-white p-2">
+    <div class="container row m-auto">
+      <div class="col">
+        <p>Пилотов</p>
+        <h3><b>74</b></h3>
+      </div>
+      <div class="col">
+        <p>Полетов</p>
+        <h3><b>18093</b></h3>
+      </div>
+      <div class="col">
+        <p>Самолетов</p>
+        <h3><b>520</b></h3>
+      </div>
+      <div class="col">
+        <p>Часов</p>
+        <h3><b>60073</b></h3>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "HomeStatsSection"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.stats {
+  background-color: #022f68;
+
+  h3 {
+    margin: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+}
+</style>
---
+++ aflva-front/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <Engine></Engine>
+  <HomeTabsSection ></HomeTabsSection>
+  <HomeStatsSection></HomeStatsSection>
+  <Online :title="'Online'"></Online>
+  <Online :title="'Booked Flights'"></Online>
+</template>
+
+<script>
+
+import Engine from "@/components/Engine";
+import HomeTabsSection from "@/components/HomeTabsSection";
+import HomeStatsSection from "@/components/HomeStatsSection";
+import Online from "@/components/Online";
+
+export default {
+  name: 'Home',
+  components: {
+    Engine, HomeTabsSection, HomeStatsSection, Online
+  }
+}
+</script>
